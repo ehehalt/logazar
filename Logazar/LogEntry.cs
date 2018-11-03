0a31e819715ecee6ec5b5482af516f2b32ec690b
Michael Ehehalt <mike42@hotmail.de>
2018-11-03 18:17:02 +0100

    LogEntry properties added

--- Logazar/LogEntry.cs
+++ Logazar/LogEntry.cs
@@ -5,12 +5,35 @@ namespace Logazar
 {
     public class LogEntry
     {
-        public IList<String> Lines { get; protected set; } = new List<String>();
-        public Boolean Parsed { get; protected set; } = false;
+        // Original lines
+        public IList<String> Lines { get; protected set; } 
+        public Boolean Parsed { get; protected set; }
+
+        // Parsed data
+        public String Data { get; protected set; }
+				public String Type { get; protected set; }
+				public String Level { get; protected set; }
+				public DateTime TimeStamp { get; protected set; }
+
+				public String TimeStampString
+				{
+						get
+						{
+								return TimeStamp.ToShortDateString() + " " + TimeStamp.ToLongTimeString();
+						}
+				}
 
         public LogEntry()
         {
+            Lines = new List<String>();
+            Parsed = false;
 
+						Data = String.Empty;
+						Type = String.Empty;
+						Level = String.Empty;
+						TimeStamp = new DateTime();
+						
+						
         }
 
         public void AddLine(String newLine)
