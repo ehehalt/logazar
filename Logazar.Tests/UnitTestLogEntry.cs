46d500bc6668363223612dda785906ae58f6ab5f
Michael Ehehalt <mike42@hotmail.de>
2018-11-04 17:33:05 +0100

    date and time from the logline parsed

--- Logazar.Tests/UnitTestLogEntry.cs
+++ Logazar.Tests/UnitTestLogEntry.cs
@@ -23,5 +23,29 @@ namespace Logazar.Tests
             Assert.NotEmpty(entry.Lines);
             Assert.Equal(1, entry.Lines.Count);
         }
+
+				[Fact]
+				public void TestConnectLine()
+				{
+						const String line1 = "10/26/18 15:09:26 1> [connect] dbname = XE username = SCOTT";
+            DateTime timeStamp1 = new DateTime(2018, 10, 26, 15, 9, 26);
+
+						var entry1 = new LogEntry();
+						entry1.AddLine(line1);
+						entry1.Parse();
+            Assert.True(entry1.Parsed);
+            Assert.Equal(1, entry1.Lines.Count);
+            Assert.True(timeStamp1.Equals(entry1.TimeStamp));
+
+						const string line2 = "11/2/18 15:09:26 1> [connect] dbname = XE username = SCOTT";
+            DateTime timeStamp2 = new DateTime(2018, 11, 2, 15, 9, 26);
+
+						var entry2 = new LogEntry();
+						entry2.AddLine(line2);
+						entry2.Parse();
+            Assert.True(entry2.Parsed);
+            Assert.Equal(1, entry2.Lines.Count);
+            Assert.True(timeStamp2.Equals(entry2.TimeStamp));
+				}
     }
 }
--- Logazar/LogEntry.cs
+++ Logazar/LogEntry.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Logazar
 {
@@ -32,14 +34,45 @@ namespace Logazar
 						Type = String.Empty;
 						Level = String.Empty;
 						TimeStamp = new DateTime();
-						
-						
         }
 
         public void AddLine(String newLine)
         {
             Lines.Add(newLine);
             Parsed = false;
+        }
+
+				public void Parse()
+				{
+						if(Lines.Count > 0)
+						{
+								// First line
+								var rxFirstLine = new Regex(@"(?<date>\d+/\d+/\d+.) (?<time>\d+:\d+:\d+)");
+								var matchFirstLine = rxFirstLine.Match(Lines.First());
+                if (matchFirstLine.Success)
+                {
+                  var date = matchFirstLine.Groups["date"].ToString();
+                  var time = matchFirstLine.Groups["time"].ToString();
+
+                  TimeStamp = CreateTimeStamp(date: date, time: time);
+                }
+						}
+
+            Parsed = true;
+				}
+
+        private DateTime CreateTimeStamp(string date, string time)
+        {
+          var dateParts = date.Split('/');
+          int year = 2000 + Convert.ToInt32(dateParts[2]);
+          int month = Convert.ToInt32(dateParts[0]);
+          int day = Convert.ToInt32(dateParts[1]);
+          var timeParts = time.Split(':');
+          int hour = Convert.ToInt32(timeParts[0]);
+          int minute = Convert.ToInt32(timeParts[1]);
+          int second = Convert.ToInt32(timeParts[2]);
+          var timeStamp = new DateTime(year, month, day, hour, minute, second);
+          return timeStamp;
         }
     }
 }
