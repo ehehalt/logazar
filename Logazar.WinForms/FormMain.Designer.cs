22f2258c0dcc5aaf34c00887691f78228a7528cd
Michael Ehehalt <mike42@hotmail.de>
2018-11-06 08:35:00 +0100

    Base design

--- Logazar.WinForms/FormMain.Designer.cs
+++ Logazar.WinForms/FormMain.Designer.cs
@@ -28,20 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripTop = new System.Windows.Forms.ToolStrip();
+            this.toolStripBottom = new System.Windows.Forms.ToolStrip();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip.TabIndex = 0;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(16, 17);
+            this.toolStripStatusLabel.Text = "...";
+            // 
+            // toolStripTop
+            // 
+            this.toolStripTop.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStripTop.Location = new System.Drawing.Point(0, 0);
+            this.toolStripTop.Name = "toolStripTop";
+            this.toolStripTop.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStripTop.Size = new System.Drawing.Size(800, 25);
+            this.toolStripTop.TabIndex = 1;
+            this.toolStripTop.Text = "...";
+            // 
+            // toolStripBottom
+            // 
+            this.toolStripBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStripBottom.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStripBottom.Location = new System.Drawing.Point(0, 403);
+            this.toolStripBottom.Name = "toolStripBottom";
+            this.toolStripBottom.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStripBottom.Size = new System.Drawing.Size(800, 25);
+            this.toolStripBottom.TabIndex = 2;
+            this.toolStripBottom.Text = "...";
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.splitContainer.Size = new System.Drawing.Size(800, 378);
+            this.splitContainer.SplitterDistance = 178;
+            this.splitContainer.TabIndex = 3;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer);
+            this.Controls.Add(this.toolStripBottom);
+            this.Controls.Add(this.toolStripTop);
+            this.Controls.Add(this.statusStrip);
             this.Name = "FormMain";
             this.Text = "Logazar";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolStrip toolStripTop;
+        private System.Windows.Forms.ToolStrip toolStripBottom;
+        private System.Windows.Forms.SplitContainer splitContainer;
     }
 }
 
